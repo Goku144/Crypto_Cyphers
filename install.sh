18b9b3be0382c06b91e0e32b5c9d42c92f7502c0
Goku144 <ytyui31@gmail.com>
2025-10-17 18:18:42 +0100

    Ubuntu

---
+++ install.sh
@@ -0,0 +1,8 @@
+cd lib-src
+sudo make install clean
+cd ..
+cd ..
+cd CLI-src
+sudo make install clean
+cd ..
+cd ..
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+cd lib-src
+sudo make uninstall clean
+cd ..
+cd ..
+cd CLI-src
+sudo make uninstall clean
+cd ..
+cd ..
