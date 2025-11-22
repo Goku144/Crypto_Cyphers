0f1ba67f9eafe01ca83ebc86bc1a3fc647a90221
Goku144 <ytyui31@gmail.com>
2025-11-22 03:50:15 +0100

    PROTOCOL

--- CLI/src/main.c
+++ CLI/src/main.c
@@ -80,33 +80,34 @@ void cy_buff_header_imp(const uint8_t buff[], struct CY_HEADER *head)
 
 void cy_buff_send(int __fd, const struct CY_HEADER head, uint8_t *buff)
 {
-    uint8_t *p = buff;
-    cy_buff_header_exp(head, p);
+    cy_buff_header_exp(head, buff);
     size_t fullsize = head.cy_data_len + 16;
     size_t total = 0;
     while (total < fullsize)
     {
-        ssize_t n = send(__fd, p + total, fullsize - total, 0);
-        if(n == 0) serror("Peer closed before we send the full msg");
-        if(n < 0) serror(__func__);
+        ssize_t n = send(__fd, buff + total, fullsize - total, 0);
+        if(n == 0) serror("cy_buff_send(-> send <-)");
+        if(n < 0) serror("cy_buff_send(-> send <-)");
         total +=n;
     }
 }
 
-void cy_buff_recv(int __fd, struct CY_HEADER *head, uint8_t *buff)
+void cy_buff_recv(int __fd, struct CY_HEADER *head, uint8_t **buff)
 {
-    uint8_t *p = buff;
     size_t fullsize = 16;
     size_t total = 0;
+    memset(head, 0, sizeof(*head));
+    *buff = malloc(CY_BUFFSIZE * sizeof(**buff));
+    if(!*buff) serror("cy_buff_read(-> malloc <-)");
     while (total < fullsize)
     {
-        ssize_t n = recv(__fd, p + total, fullsize - total, 0);
-        if(n == 0) serror("Peer closed before we recv the full msg");
-        if(n < 0) serror(__func__);
+        ssize_t n = recv(__fd, *buff + total, fullsize - total, 0);
+        if(n == 0) serror("cy_buff_recv(-> recv <-)");
+        if(n < 0) serror("cy_buff_recv(-> recv <-)");
         total +=n;
         if(total == 16)
         {
-            cy_buff_header_imp(buff, head);
+            cy_buff_header_imp(*buff, head);
             fullsize = head->cy_data_len + total;
         }
     }
@@ -117,18 +118,18 @@ void cy_buff_read(int __fd, struct CY_HEADER *head, uint8_t **buff)
     size_t buffsize = CY_BUFFSIZE;
     memset(head, 0, sizeof(*head));
     *buff = malloc(CY_BUFFSIZE * sizeof(**buff));
-    if(!*buff) serror(__func__);
+    if(!*buff) serror("cy_buff_read(-> malloc <-)");
     while (1)
     {
         if(buffsize - head->cy_data_len < CY_BUFFSIZE) 
         {
             buffsize += CY_BUFFSIZE;
             *buff = realloc(*buff, buffsize * sizeof(*buff));
-            if(!*buff) serror(__func__);
+            if(!*buff) serror("cy_buff_read(-> realloc <-)");
         }
-        ssize_t n = read(__fd, *buff + head->cy_data_len, CY_BUFFSIZE);
+        ssize_t n = read(__fd, *buff + head->cy_data_len + 16, CY_BUFFSIZE - 16);
         if(n == 0) return;
-        if(n < 0) serror(__func__);
+        if(n < 0) serror("cy_buff_read(-> read <-)");
         head->cy_data_len += n;
     }
 }
@@ -140,8 +141,8 @@ void cy_buff_write(int __fd, const struct CY_HEADER head, const uint8_t *buff)
     while (total < fullsize)
     {
         ssize_t n = write(__fd, buff + total, fullsize - total);
-        if(n == 0) serror("Peer closed before we send the full msg");
-        if(n < 0) serror(__func__);
+        if(n == 0) serror("cy_buff_write(-> write <-)");
+        if(n < 0) serror("cy_buff_write(-> write <-)");
         total +=n;
     }
 }
@@ -160,12 +161,72 @@ void print_u128_hex(__uint128_t x) {
     printf("0x%016llx%016llx\n", hi, lo);
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    struct CY_HEADER hmida;
-    uint8_t *buff;
-    int fd = open("hmi.txt", O_RDWR, 777);
-    cy_buff_read(fd, &hmida, &buff);
-    cy_buff_write(STDOUT_FILENO, hmida, buff);
+    if (argc < 3) {
+        fprintf(stderr, "Usage:\n  %s -sp <port>      (server)\n  %s <host> <port>   (client)\n",
+                argv[0], argv[0]);
+        return 1;
+    }
+
+    struct sockaddr_in client_addr;
+    struct addrinfo hints, *servinfo = NULL, *clientinfo = NULL;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_INET;        // IPv4
+    hints.ai_socktype = SOCK_STREAM;    // TCP
+
+    if (!strcmp(argv[1], "-sp")) {
+        // -------- SERVER MODE --------
+        hints.ai_flags = AI_PASSIVE;
+
+        if (getaddrinfo(NULL, argv[2], &hints, &servinfo) != 0)
+            return 1;  // forget detailed error handling
+
+        int sd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+        if (sd < 0) return 1;
+
+        if (bind(sd, servinfo->ai_addr, servinfo->ai_addrlen) < 0) return 1;
+        if (listen(sd, 1) < 0) return 1;
+
+        printf("listening ...\n");
+
+        socklen_t addr_size = sizeof client_addr;
+        int new_fd = accept(sd, (struct sockaddr *)&client_addr, &addr_size);
+
+        if (new_fd == -1)
+            printf("Connection was unsuccessfull\n");
+        else
+            printf("Connection was successful\n");
+        struct CY_HEADER head; uint8_t *buff;
+        cy_buff_recv(new_fd, &head, &buff);
+        cy_buff_write(STDOUT_FILENO, head, buff + CY_HEADER_OFFSET);
+
+        // clean up
+        close(new_fd);
+        close(sd);
+        freeaddrinfo(servinfo);
+
+    } else {
+        // -------- CLIENT MODE --------
+        if (getaddrinfo(argv[1], argv[2], &hints, &clientinfo) != 0)
+            return 1;
+
+        int sd = socket(clientinfo->ai_family, clientinfo->ai_socktype, clientinfo->ai_protocol);
+        if (sd < 0) return 1;
+
+        if (connect(sd, clientinfo->ai_addr, clientinfo->ai_addrlen) < 0)
+            printf("Connection failed\n");
+        else
+            printf("Connected to server\n");
+        struct CY_HEADER head;
+        uint8_t *buff;
+        cy_buff_read(STDIN_FILENO, &head, &buff);
+        cy_buff_send(sd, head, buff);
+
+        close(sd);
+        freeaddrinfo(clientinfo);
+    }
+
     return 0;
 }
